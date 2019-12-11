e5df2f6a63a738b0f67b39e682616de969a7f95d
Claudio Busatto <cjcbusatto@gmail.com>
2019-12-11 10:53:08 +0100

    Include more tests

--- yassers-challenge-1/solution.test.js
+++ yassers-challenge-1/solution.test.js
@@ -1,12 +1,21 @@
 const solution = require('./solution');
 
-test('[] to equal 3', () => {
+test('[] to be equal to 1', () => {
   expect(solution([])).toBe(1);
 });
 
-test('[1, 3, 6, 4, 1, 2] to equal 5', () => {
+test('[1, 3, 6, 4, 1, 2] to be equal to 5', () => {
   expect(solution([1, 3, 6, 4, 1, 2])).toBe(5);
 });
-test('[1, 2, 3] to equal 4', () => {
+
+test('[1, 2, 3] to be equal to 4', () => {
   expect(solution([1, 2, 3])).toBe(4);
+});
+
+test('[1,2,3,4,5,6] to be equal to 7', () => {
+  expect(solution([1, 2, 3, 4, 5, 6])).toBe(7);
+});
+
+test('[2,4,3] to be equal to 1', () => {
+  expect(solution([2, 4, 3])).toBe(1);
 });
