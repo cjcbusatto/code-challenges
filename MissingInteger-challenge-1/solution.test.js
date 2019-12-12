fa444e224146f8b35a1b48fbc09c0c0ee53325c6
Claudio Busatto <cjcbusatto@gmail.com>
2019-12-12 16:43:43 +0100

    Test with not linted code

--- MissingInteger-challenge-1/solution.test.js
+++ MissingInteger-challenge-1/solution.test.js
@@ -5,7 +5,6 @@ const solutionB = require('./solutionB');
 function shuffleArray(arr) {
   for (let i = arr.length - 1; i > 0; i -= 1) {
     const j = Math.floor(Math.random() * (i + 1));
-    // eslint-disable-next-line no-param-reassign
     [arr[i], arr[j]] = [arr[j], arr[i]];
   }
   return arr;
