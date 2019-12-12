902374a322f26df7f150088b4eebbc883681f02a
Claudio Busatto <cjcbusatto@gmail.com>
2019-12-12 16:55:35 +0100

    Committing a file fixing linting problems

--- MissingInteger-challenge-1/solution.test.js
+++ MissingInteger-challenge-1/solution.test.js
@@ -5,8 +5,10 @@ const solutionB = require('./solutionB');
 function shuffleArray(arr) {
   for (let i = arr.length - 1; i > 0; i -= 1) {
     const j = Math.floor(Math.random() * (i + 1));
+    // eslint-disable-next-line no-param-reassign
     [arr[i], arr[j]] = [arr[j], arr[i]];
   }
+
   return arr;
 }
 
